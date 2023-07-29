9832fe73afd0f2ea502c9700a23862cb818ff05f
julius <juliu@LAPTOP-GK4S5PN6>
2023-07-29 10:36:17 +0200

    Add project files.

---
+++ sommar_hobbyprojektet/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace sommar_hobbyprojektet;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ sommar_hobbyprojektet/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace sommar_hobbyprojektet;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
