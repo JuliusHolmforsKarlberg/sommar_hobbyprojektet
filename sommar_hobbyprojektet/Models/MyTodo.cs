83da3fece5d6d46229ccbf9ca317c240618a33ca
julius <juliu@LAPTOP-GK4S5PN6>
2023-08-05 11:16:03 +0200

    Skapat create page #7

---
+++ sommar_hobbyprojektet/Models/MyTodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sommar_hobbyprojektet.Models
+{
+    public class MyTodo
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime TodoDate { get; set; } 
+        public bool IsDone { get; set; }
+    }
+}
